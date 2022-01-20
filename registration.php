74299daa13b23ce702902e9869505b69b8f8f78e
Opauszki László <tlsoftlacca@gmail.com>
2022-01-20 18:02:38 +0100

    First commit.
    Took 55 seconds

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2021 Ecommsolvers. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'TLSoft_BarionGateway', __DIR__);
