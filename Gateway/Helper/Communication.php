54b423295c72fbb90d7bf9e2b60a71df9e4b62a3
Opauszki László <tlsoftlacca@gmail.com>
2024-11-21 11:34:03 +0100

    Id fix.
    Took 4 minutes

--- Gateway/Helper/Communication.php
+++ Gateway/Helper/Communication.php
@@ -149,9 +149,9 @@ class Communication extends AbstractHelper
         $methodCode = "bariongateway";
 
         if(!array_key_exists("paymentId",$params[0]))
-            return
+            return;
 
-        $transaction_id = $params[0]["paymentId"];
+        $transactionId = $params[0]["paymentId"];
 
         if (!$order)
             $order = $this->checkoutSession->getLastRealOrder();
@@ -180,11 +180,11 @@ class Communication extends AbstractHelper
 
         $config = $this->getProviderConfig($methodCode);
 
-        $params = "?POSKey=" . $this->getPosKey($config) . "&PaymentId=" . $transaction_id;
+        $params = "?POSKey=" . $this->getPosKey($config) . "&PaymentId=" . $transactionId;
 
         $this->responseCode = ResultCodes::RESULT_PENDING;
 
-        $resulttext = __('Transaction ID') . ": " . $transaction_id . "\n";
+        $resulttext = __('Transaction ID') . ": " . $transactionId . "\n";
 
         $response = $this->cURL($helper->getStateUrl() . $params);
 
@@ -207,12 +207,12 @@ class Communication extends AbstractHelper
                     $this->responseCode = ResultCodes::RESULT_SUCCESS;
                     $this->messageManager->addSuccessMessage($resulttext);
                     $payment->setIsTransactionApproved(true);
-                    $payment->setLastTransId($transaction_id);
+                    $payment->setLastTransId($transactionId);
                     $payment->setAdditionalInformation([Transaction::RAW_DETAILS => $result]);
 
                     $transaction = $this->transactionBuilder->setPayment($payment)
                         ->setOrder($order)
-                        ->setTransactionId($transaction_id)
+                        ->setTransactionId($transactionId)
                         ->setFailSafe(true)
                         ->setAdditionalInformation([Transaction::RAW_DETAILS => $result])
                         ->build(TransactionInterface::TYPE_CAPTURE);
@@ -234,7 +234,7 @@ class Communication extends AbstractHelper
 
                     $transaction = $this->transactionBuilder->setPayment($payment)
                         ->setOrder($order)
-                        ->setTransactionId($transaction_id)
+                        ->setTransactionId($transactionId)
                         ->setFailSafe(true)
                         ->setAdditionalInformation([Transaction::RAW_DETAILS => $result])
                         ->build(TransactionInterface::TYPE_ORDER);
@@ -255,7 +255,7 @@ class Communication extends AbstractHelper
 
                     $transaction = $this->transactionBuilder->setPayment($payment)
                         ->setOrder($order)
-                        ->setTransactionId($transaction_id)
+                        ->setTransactionId($transactionId)
                         ->setFailSafe(true)
                         ->setAdditionalInformation([Transaction::RAW_DETAILS => $result])
                         ->build(TransactionInterface::TYPE_ORDER);
@@ -273,7 +273,7 @@ class Communication extends AbstractHelper
 
                     $transaction = $this->transactionBuilder->setPayment($payment)
                         ->setOrder($order)
-                        ->setTransactionId($transaction_id)
+                        ->setTransactionId($transactionId)
                         ->setFailSafe(true)
                         ->setAdditionalInformation([Transaction::RAW_DETAILS => $result])
                         ->build(TransactionInterface::TYPE_ORDER);
@@ -299,7 +299,7 @@ class Communication extends AbstractHelper
 
                 $transaction = $this->transactionBuilder->setPayment($payment)
                     ->setOrder($order)
-                    ->setTransactionId($transaction_id)
+                    ->setTransactionId($transactionId)
                     ->setFailSafe(true)
                     ->setAdditionalInformation([Transaction::RAW_DETAILS => $result])
                     ->build(TransactionInterface::TYPE_ORDER);
